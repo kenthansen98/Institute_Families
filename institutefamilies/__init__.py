fd2ed9bfc3eeffd7755e451a891cd72f6706c291
Kent Hansen <kenthansen@Kents-MacBook-Air.local>
2019-08-09 22:28:21 -0500

    changed database URI

--- institutefamilies/__init__.py
+++ institutefamilies/__init__.py
@@ -1,9 +1,10 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'e2541bd6b619d89f63257496f6b158cc'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
 from institutefamilies import routes
