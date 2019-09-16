913a9a04829a3d9ae5696d269b8647a54725a2bc
Kent Hansen <kenthansen@wireless-10-148-205-96.public.utexas.edu>
2019-09-16 12:10:17 -0500

    deleted uneccesary secret key

--- institutefamilies/__init__.py
+++ institutefamilies/__init__.py
@@ -3,7 +3,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'e2541bd6b619d89f63257496f6b158cc'
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
